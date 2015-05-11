a7990e6ca49d95dcf57ec5ba9d9ae19308f7ddb9
waalsh <waalsh@mit.edu>
2015-05-10 21:47:31 -0400

    most of other-image implementation

--- models.scm
+++ models.scm
@@ -4,21 +4,73 @@
   (list 'weak))
 
 (define (estimate-diplomacy country actions)
-  (let action-loop ((actions actions))
+  (if (> (gift-counter actions) (/ (length actions) 2))
+      (list 'isolationist)
+      (list 'interventionist)))
+
+(define (gift-counter actions)
+  (let action-loop ((actions actions)
+		    (counter 0))
     (cond ((pair? actions)
-	   (pp (car (car actions)))
-	   (action-loop (cdr actions))))
-    (list 'isolationist)))
+	   (if (eq? 'gift (car (car actions)))
+	       (action-loop (cdr actions) (+ counter 1))
+	       (action-loop (cdr actions) counter)))
+	 (else counter))))
+	
+(define (estimate-aggression country country-watching actions)
+  (let ((war-count (war-count country actions))
+	(war-of-aggression (find-war country country-watching actions)))
+    (cond (war-of-aggression
+	   (list 'violent))
+	  ((> war-count 0)
+	   (list 'aggressive))
+	  ((> (gift-counter actions) (/ (length actions) 2))
+	   (list 'weak))
+	  (else (list 'rational)))))
 
-(define (estimate-aggression country actions) ;;we need to improve this
-  (list 'meek))
+(define (war-count country actions)
+  (let action-loop ((actions actions)
+		    (counter 0))
+    (cond ((pair? actions)
+	   (if (eq? 'attack (car (car actions)))
+	       (action-loop (cdr actions) (+ counter 1))
+	       (action-loop (cdr actions) counter)))
+	 (else counter))))
 
-(define (estimate-confidence country actions) ;;we need to improve this
-  (list 'critical))
+(define (find-war country country-watching actions)
+  (let action-loop ((actions actions)
+		    (aggression #f))
+    (cond ((pair? actions)
+	   (if (eq? country-watching (caddr (car actions)))
+	       #t
+	       (action-loop (cdr actions) #f)))
+	  (else aggression))))
+
+(define (estimate-confidence country country-watching actions)
+  (let ((gifts-to-weaker (gifts-to-weaker country actions))
+	(war-of-aggression (find-bad-war country actions)))
+    (cond (war-of-aggression
+	   (list 'conceited))
+	  ((and gifts-to-weaker ())
+	   (list 'critical))
+	  (else (list 'realistic)))))
+
+(define (gifts-to-weaker country actions)
+  (let action-loop ((actions actions)
+		    (gifts? #f))
+    (cond ((pair? actions)
+	   (cond ((eq? 'gift (car (car actions)))
+		  (if (weaker? (caddr (car actions)) country)
+		      #t
+		      (else (action-loop (cdr actions) #f)))
+		  (else (action-loop (cdr actions) #f)))))
+	  (else gifts))))
+
+(define (weaker? country1 country2) ;change after Keke
+  #t)
 
 (define (estimate-intelligence country actions) ;;we need to improve this
   (list 'follower))
-
 
 ;find-diplomatic-opinions! decides which traits other countries should have
 ;output: dictionary of a country
@@ -28,11 +80,11 @@
   (let ((opinions '()))
     (let country-loop ((countries all-countries)
 		       (c-character '()))
-      (cond ((pair? countries)
+       (cond ((pair? countries)
 	     (set! c-character (append c-character (list (car countries))))
-	     (set! c-character (append c-character (estimate-aggression (car countries) (actions-taken (car countries)))))
+	     (set! c-character (append c-character (estimate-aggression (car countries) country-with-opinions (actions-taken (car countries)))))
 	     (set! c-character (append c-character (estimate-diplomacy (car countries) (actions-taken (car countries)))))
-	     (set! c-character (append c-character (estimate-confidence (car countries) (actions-taken (car countries)))))
+	     (set! c-character (append c-character (estimate-confidence (car countries) country-with-opinions (actions-taken (car countries)))))
 	     (set! c-character (append c-character (estimate-strength (car countries) (actions-taken (car countries)))))
 	     (set! c-character (append c-character (estimate-intelligence (car countries) (actions-taken (car countries)))))
 	  
@@ -65,14 +117,23 @@
 			      3.8
 			      300))
 
+;Catching Diplomacy
 (define (past-canada-actions)
-	(list (list 'attack canada usa) (list 'gift canada usa) (list 'gift canada usa)))
+	(list (list 'gift canada usa) (list 'gift canada usa) (list 'attack canada usa)))
 
+
+
+(gift-counter past-usa-actions)
 (define (past-usa-actions)
-	(list (list 'attack usa canada) (list 'gift usa canada) (list 'gift usa canada)))
+	(list 
+	 (list 'gift usa canada) 
+	 (list 'gift usa canada)))
 
 (set-actions-taken! canada (past-canada-actions))
 (set-actions-taken! usa (past-usa-actions))
 
-(set-diplomatic-opinions! usa (find-diplomatic-opinions usa (list usa canada usa usa)))
+(set-diplomatic-opinions! usa (find-diplomatic-opinions usa (list canada usa)))
 (diplomatic-opinions usa)
+
+(set-self-image! usa (inherent-traits usa))
+(image usa)
